95ea687dd89b09e8f48bebcffe683a9a099dcfe1
eezzo <derastafari@gmail.com>
2019-03-10 17:29:10 -0600

    Added polls app

--- djangotutorial/settings.py
+++ djangotutorial/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #custom apps
+    'polls.apps.PollsConfig',
+    #Default apps 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
